2f5d179723a835611c8ac4f9e204ba07907551e4
matugm <matugm@gmail.com>
2015-08-01 22:08:48 +0200

    Refactor controller test

--- spec/controllers/tag_spec.rb
+++ spec/controllers/tag_spec.rb
@@ -1,38 +1,48 @@
 require 'rails_helper'
 
-describe TagsController, "#show -> GET /tags/:tag_name" do
-  let!(:books) { create_books }
+describe TagsController do
+  # GET /tags/:tag_name
+  describe "#show" do
 
-  before { get :show, tag_name: 'Ruby' }
+    context "when books are found" do
+      before { create_books }
 
-  it 'renders the show template with a success response code' do
-    expect(response).to have_http_status(200)
-    expect(response).to render_template(:show)
-  end
+      it 'renders the show template with a success response code' do
+        get :show, tag_name: 'Ruby'
+        expect(response).to have_http_status(200)
+        expect(response).to render_template(:show)
+      end
 
-  it 'assigns the filtered books to @books' do
-    expect(assigns(:books).first).to be_an_instance_of(Book)
-  end
-
-  context "when there are no books with this tag" do
-    before { get :show, tag_name: 'Rspec' }
-
-    # Render the view to make sure it doesn't crash when given an empty @books
-    render_views
-
-    it 'assigns an empty list of records to @books' do
-      expect(assigns(:books)).to be_nil
-      expect(response).to have_http_status(200)
+      it 'assigns the filtered books to @books' do
+        get :show, tag_name: 'Ruby'
+        expect(assigns(:books)).to_not be_nil
+      end
     end
-  end
 
-  private
+    context "when there are no books with this tag" do
+       # Render the view to make sure it can handle an empty @books object
+      render_views
 
-  def create_books
-    ruby_tag = [Tag.create(name: 'Ruby')]
+      it 'renders the show template with a success response code' do
+        get :show, tag_name: 'Rspec'
+        expect(response).to have_http_status(200)
+        expect(response).to render_template(:show)
+      end
 
-    Book.create(title: "Eloquent Ruby", tags: ruby_tag)
-    Book.create(title: "The Well-Grounded Rubyist", tags: ruby_tag)
-    Book.create(title: "Practical Object-Oriented Design in Ruby", tags: ruby_tag)
+      it 'assigns an empty list of records to @books' do
+        get :show, tag_name: 'Rspec'
+        expect(assigns(:books)).to be_nil
+      end
+    end
+
+    private
+
+    def create_books
+      ruby_tag = [Tag.create(name: 'Ruby')]
+
+      Book.create(title: "Eloquent Ruby", tags: ruby_tag)
+      Book.create(title: "The Well-Grounded Rubyist", tags: ruby_tag)
+      Book.create(title: "Practical Object-Oriented Design in Ruby", tags: ruby_tag)
+    end
   end
 end
