674d2107ffdce5375d8bc0fd024f68e5bcd77162
matugm <matugm@gmail.com>
2016-02-25 23:09:43 +0100

    Inline tag name

--- spec/features/tag_search_spec.rb
+++ spec/features/tag_search_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 feature "Tag search" do
-  let(:tag_name)  { tag(:ruby).name }
   let(:ruby_book) { book(:eloquent).title }
 
   scenario "Users can filter post by tag by clicking on a tag name" do
     visit books_path
-    click_link(tag_name, match: :first)
+
+    click_link('Ruby', match: :first)
 
     expect(page).to have_text(ruby_book)
   end
